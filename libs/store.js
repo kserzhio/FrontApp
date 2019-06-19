e7c6fbcfc9868add21f43a91a39573c60e7bed09
kserzhio <kserzhio@gmail.com>
2019-06-19 20:17:27 +0300

    front app

---
+++ libs/store.js
@@ -0,0 +1,11 @@
+import {createStore,applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+import reducers from './reducers'
+
+const middlewares = [];
+
+export default () => createStore(
+    reducers,undefined,
+    composeWithDevTools(applyMiddleware(...middlewares ))
+);
---
+++ pages/a.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { graphql, compose } from 'react-apollo';
+import gql from 'graphql-tag';
+
+class A extends Component {
+    render() {
+        const {
+            // login,
+            data: { loading, hi }
+        } = this.props;
+        if (loading) {
+            return <div>loading...</div>;
+        }
+        return <div>{hi}</div>;
+    }
+}
+
+const hiQuery = gql`
+  {
+    hi
+  }
+`;
+
+const mapStateToProps = state => ({
+    login: state.me.login
+});
+
+const mapDispatchToProps = {};
+
+export default compose(
+    graphql(hiQuery),
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    )
+)(A);
